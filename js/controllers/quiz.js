6c6a8cbdc6be78eb3660daef5118c7855497284f
Alan Campbell <alancampbell4444@gmail.com>
2016-11-30 17:57:37 -0800

    quiz markup and logic, data factory

---
+++ js/controllers/quiz.js
@@ -0,0 +1,44 @@
+(function(){
+ 
+    angular
+        .module("turtleFacts")
+        .controller("quizCtrl", QuizController);
+
+    QuizController.$inject = ['quizMetrics', 'DataService']
+ 
+    function QuizController(quizMetrics, DataService){
+        var vm = this;
+        vm.quizMetrics=quizMetrics;
+        vm.dataService=DataService;
+        vm.activeQuestion=0;
+        vm.questionAnswered=questionAnswered;
+        var numQuestionsAnswered=0;
+        vm.setActiveQuestion = setActiveQuestion;
+
+       function setActiveQuestion(){
+    		var breakOut = false;
+    		var quizLength = DataService.quizQuestions.length - 1;
+ 			while(!breakOut){
+        		 vm.activeQuestion = vm.activeQuestion < quizLength?++vm.activeQuestion:0;
+        		 if(DataService.quizQuestions[vm.activeQuestion].selected === null){
+        			breakOut = true;
+    			}
+    		}
+		}
+
+
+		function questionAnswered(){
+ 			var quizLength = DataService.quizQuestions.length;
+ 			if(DataService.quizQuestions[vm.activeQuestion].selected !== null){
+        		numQuestionsAnswered++;
+        		if(numQuestionsAnswered >= quizLength){
+           			//Finalise the quiz
+        		}
+    		}
+    		vm.setActiveQuestion();
+ 
+		}
+       
+    }
+
+})();
